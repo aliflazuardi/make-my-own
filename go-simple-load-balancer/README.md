fa6ced198132db4deec73e5f25800d84aa783ab7
aliflazuardi <aliflazuardi250999@gmail.com>
2024-07-20 21:51:37 +0700

    add go-simple-load-balancer server and server pool structs

---
+++ go-simple-load-balancer/README.md
@@ -0,0 +1,6 @@
+#Simple Load Balancer
+
+Simple load balancer using Go
+
+Load balancing strategy:
+ * Round Robin
--- go-simple-load-balancer/main.go
+++ go-simple-load-balancer/main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"net/http/httputil"
+	"net/url"
+	"sync"
+	"sync/atomic"
+)
+
+type Server struct {
+	URL          *url.URL
+	ReverseProxy *httputil.ReverseProxy
+	mux          sync.RWMutex
+	Alive        bool
+}
+
+type ServerPool struct {
+	servers []*Server
+	current uint64
+}
+
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.servers)))
+}
+
 func main() {
 }
