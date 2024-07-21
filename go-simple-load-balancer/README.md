6582d4dfb64272b55f81a035934b00798ecb1f68
aliflazuardi <aliflazuardi250999@gmail.com>
2024-07-21 19:40:39 +0700

    add mechanism to select next alive server

--- go-simple-load-balancer/README.md
+++ go-simple-load-balancer/README.md
@@ -4,3 +4,6 @@ Simple load balancer using Go
 
 Load balancing strategy:
  * Round Robin
+
+
+project reference: https://kasvith.me/posts/lets-create-a-simple-lb-go/
--- go-simple-load-balancer/main.go
+++ go-simple-load-balancer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"sync"
@@ -19,9 +20,48 @@ type ServerPool struct {
 	current uint64
 }
 
+// Avoid race conditions
+func (s *Server) SetAlive(alive bool) {
+	s.mux.Lock()
+	s.Alive = alive
+	s.mux.Unlock()
+}
+
+func (s *Server) isAlive() (alive bool) {
+	s.mux.RLock()
+	alive = s.Alive
+	s.mux.RUnlock()
+	return
+}
+
 func (s *ServerPool) NextIndex() int {
 	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.servers)))
 }
 
+// returns next active peer to take a connection
+func (s *ServerPool) GetNextPeer() *Server {
+	// Loop entire backend to search for alive server
+	next := s.NextIndex()
+	l := len(s.servers) + next // start from next index and move full cycle
+
+	for i := next; i < l; i++ {
+		idx := i % len(s.servers) // take an index by modding with length
+
+		if s.servers[idx].Alive {
+			if i != next { // original one
+
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.servers[idx]
+		}
+	}
+	return nil
+}
+
 func main() {
+	u, _ := url.Parse("http://localhost:8080")
+
+	rp := httputil.NewSingleHostReverseProxy(u)
+
+	http.HandlerFunc(rp.ServeHTTP)
 }
