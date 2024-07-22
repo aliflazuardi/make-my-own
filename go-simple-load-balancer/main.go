5973d888ef325856cbdd6c2b4f31e0b931c7742e
aliflazuardi <aliflazuardi250999@gmail.com>
2024-07-22 21:20:40 +0700

    add load balancing logic with reverse proxy

--- go-simple-load-balancer/main.go
+++ go-simple-load-balancer/main.go
@@ -58,6 +58,16 @@ func (s *ServerPool) GetNextPeer() *Server {
 	return nil
 }
 
+func lb(w http.ResponseWriter, r *http.Request) {
+	peer := serverPool.GetNextPeer()
+
+	if peer != nil {
+		peer.ReverseProxy.ServeHTTP(w, r)
+		return
+	}
+	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+}
+
 func main() {
 	u, _ := url.Parse("http://localhost:8080")
 
